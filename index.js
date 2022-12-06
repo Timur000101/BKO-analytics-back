26ee7f254b31ecaef7ed309c34c8c8fdbbe4b824
Temur <tima000101@gmail.com>
2022-12-07 01:16:40 +0600

    fixed

--- index.js
+++ index.js
@@ -27,16 +27,12 @@ mongoose.connect(DB_PATH)
 // Create pdf file
 const sendDocUrl = `https://api.telegram.org/bot5705800519:AAG2ckG_x3FQN8iLQpyAbcdhZUjy3hbQi_4/sendDocument`;
 
-// if (process.env.NTBA_FIX_350) {
-// 	contentType = contentType || 'application/octet-stream';
-// }
-
 const data = [
 	{
 		"headers": [
-			{ "label":"#", "property":"title", "width":100 },
-			{ "label":"Как сейчас", "property":"now", "width":100 },
-			{ "label":"Как должно быть", "property":"after", "width":100 }
+			{ "label":"#", "property":"title", "width": 100 },
+			{ "label":"Как сейчас", "property":"now", "width": 120 },
+			{ "label":"Итоги", "property":"after", "width": 300 }
 		],
 		"rows": [],
 		"options": {
@@ -73,25 +69,80 @@ async function sendResultTable (chatId) {
 	doc.pipe(fs.createWriteStream("./document.pdf"));
 	const user = await User.findOne({ userId: chatId })
 	const shouldBePurchaseSum = user.turnover * ((100 - user.markup) / 100)
-	const shouldBeCompanyProfit = (user.turnover - shouldBePurchaseSum ) - user.shouldBeExpenditure
-	const shouldBeNumberEmployees = user.numberEmployees - user.turnover / user.shouldBeOneEmployeeCost > 0 ? String(Math.floor(user.numberEmployees - user.turnover / user.shouldBeOneEmployeeCost)) : user.numberEmployees
-	console.log(shouldBeNumberEmployees)
-	const oneEmployeeCost = String(Math.floor(user.turnover / user.numberEmployees))
-	console.log(oneEmployeeCost)
-	const shouldBeOwnerProfit = +user.turnoverDeviation + +user.expenditureDeviation + +user.salaryEmployeesDeviation + +user.margin - +user.expenditure
-	data[0].rows.push(
+	const oneEmployeeCost = Math.floor(user.turnover / user.numberEmployees)
+	const factPurchaseSum = (user.turnover * ((100 - user.markup) / 100))
+	const factSalaryEmployees = user.companyProfit * 0.3
+	// Закуп
+	let purchaseSumResult = null
+	if (+user.turnoverDeviation > 1000000) {
+		purchaseSumResult = `Закуп должен быть на сумму ${splitNumber(shouldBePurchaseSum, true)}, у Вас отклонение ${splitNumber(user.purchaseSum  - factPurchaseSum, true)}`
+	} else if (+user.turnoverDeviation < -1000000) {
+		purchaseSumResult = 'У Вас на закуп тратиться меньше денег'
+	} else if (+user.turnoverDeviation <= 1000000 && +user.turnoverDeviation >= -1000000) {
+		purchaseSumResult = 'У Вас хорошо с закупом'
+	}
+	// Стоимость одного сотрудника
+	let oneEmployeeCostResult = null
+	if (oneEmployeeCost >= +user.shouldBeOneEmployeeCost) {
+		oneEmployeeCostResult = 'Все хорошо'
+	} else if (oneEmployeeCost >= 3000000) {
+		oneEmployeeCostResult = 'Норма'
+	} else {
+		oneEmployeeCostResult = `У Вас стоимость одного сотрудника ${splitNumber(oneEmployeeCost, true)}, должно быть 5 000 000 тг`
+	}
+	// Количество сотрудников
+	let numberEmployeesResult = null
+	if (oneEmployeeCost >= 3000000) {
+		numberEmployeesResult = user.numberEmployees
+	} else {
+		numberEmployeesResult = `У Вас должны быть ${Math.floor(user.turnover / user.shouldBeOneEmployeeCost)} сотрудников`
+	}
+	// ЗП сотрудников
+	let salaryEmployeesResult = null
+	if (user.salaryEmployees > factSalaryEmployees + factSalaryEmployees * 0.1) {
+		salaryEmployeesResult = `Зарплата сотрудников должно быть ${factSalaryEmployees}, Вы в минусе ${splitNumber(user.salaryEmployees - factSalaryEmployees, true)}`
+	} else if (user.salaryEmployees < factSalaryEmployees + factSalaryEmployees * 0.1) {
+		salaryEmployeesResult = `У Вас недомотивированные сотрудники`
+	} else {
+		salaryEmployeesResult = 'Все отлично'
+	}
+	
+	
+	
+	const expenditureResult = user.margin * 0.2 < user.expenditure ? `У Вас отклонение ${splitNumber(user.expenditure - user.margin * 0.2, true)}` : 'Расход норма'
+	
+	let shouldBeOwnerProfit = 0
+	if (+user.turnoverDeviation > 0) {
+		shouldBeOwnerProfit = shouldBeOwnerProfit + +user.turnoverDeviation
+	}
+	if (+user.expenditureDeviation > 0) {
+		shouldBeOwnerProfit = shouldBeOwnerProfit + +user.expenditureDeviation
+	}
+	if (+user.salaryEmployeesDeviation > 0) {
+		shouldBeOwnerProfit = shouldBeOwnerProfit + +user.salaryEmployeesDeviation
+	}
+	
+	let ownerProfitResult = null
+	if (+user.ownerProfit < shouldBeOwnerProfit + +user.ownerProfit) {
+		ownerProfitResult = `Вы теряете ${splitNumber(shouldBeOwnerProfit, true)} в месяц`
+	} else {
+		ownerProfitResult = 'Все отлично'
+	}
+	
+	console.log(shouldBeOwnerProfit)
+	
+ 	data[0].rows.push(
 		['Оборот', splitNumber(user.turnover, true), splitNumber(user.turnover, true)],
-		['Наценка', splitNumber(user.markup, false), splitNumber(user.markup, false)],
-		['Закуп', splitNumber(user.purchaseSum, true), splitNumber(shouldBePurchaseSum, true)],
-		['Маржа', splitNumber(user.margin, true), splitNumber(user.turnover - shouldBePurchaseSum, true)],
-		['Расходы', splitNumber(user.expenditure, true), splitNumber(user.shouldBeExpenditure, true)],
-		['Приб. компании', splitNumber(user.companyProfit, true), splitNumber(shouldBeCompanyProfit, true)],
-		// ['Кол. сотрд', splitNumber(user.numberEmployees, false), splitNumber(shouldBeNumberEmployees, false)],
-		// ['Стоимость одного сотрудника', splitNumber(oneEmployeeCost, true), splitNumber(user.shouldBeOneEmployeeCost, true)]
-		['ЗП сотрд', splitNumber(user.salaryEmployees, true), splitNumber(user.shouldBeSalaryEmployees, true)],
-		['Приб. владельца', splitNumber(user.ownerProfit, true), splitNumber(shouldBeOwnerProfit, true)]
+		['Закуп', splitNumber(user.purchaseSum, true), `${purchaseSumResult}`],
+		['Маржа', splitNumber(user.margin, true), ''],
+		['Расходы', splitNumber(user.expenditure, true), `${expenditureResult}`],
+		['Приб. компании', splitNumber(user.companyProfit, true), ''],
+		['Стоимость одного сотрудника', splitNumber(oneEmployeeCost, true), `${oneEmployeeCostResult}`],
+		['Кол. сотрд', user.numberEmployees, `${numberEmployeesResult}`],
+		['ЗП сотрд', splitNumber(user.salaryEmployees, true), `${salaryEmployeesResult}`],
+		['Приб. владельца', splitNumber(user.ownerProfit, true), `${ownerProfitResult}`]
 	)
-
+	
 	doc.table(data[0], {
 		prepareHeader: () => doc.font(localType).fontSize(10),
 		prepareRow: () => doc.font(localType).fontSize(10)
@@ -115,8 +166,8 @@ bot.on('message', async (msg) => {
 		await bot.sendMessage(chatId,  `Ниже появится кнопка "Аналитика", заполните форму.\nУслуга стоит 20 000 тг`, { reply_markup: {
 				keyboard: [
 					[ { text: 'Аналитика', web_app: { url: webAppUrl }} ],
-					[ { text: 'Договор оферты', web_app: { url: 'https://drive.google.com/file/d/1FQLGqLqxYwuAZGBbg8vTzh1iwKF9arJN/view?usp=share_link' }} ],
-					[ { text: 'Политика конфиденциальности', web_app: { url: 'https://drive.google.com/file/d/1P3nFJzK7whSRNAZ-TWSnSjyUGoMzgG0T/view?usp=share_link' } } ]
+					[ { text: 'Договор оферты', web_app: { url: 'https://docs.google.com/document/d/16Qohr9PgbMjoravVdGpne3eCtq_w6wXr/edit?usp=share_link&ouid=114431697732675672393&rtpof=true&sd=true' }} ],
+					[ { text: 'Политика конфиденциальности', web_app: { url: 'https://docs.google.com/document/d/1LXePS0S3vVcl9JQ3ByU-Twnnj4mpivX2/edit?usp=share_link&ouid=114431697732675672393&rtpof=true&sd=true' } } ]
 				],
 				resize_keyboard: true,
 				selective: true
@@ -126,7 +177,6 @@ bot.on('message', async (msg) => {
 	if(msg?.web_app_data?.data) {
 		try {
 			const data = JSON.parse(msg?.web_app_data?.data)
-			await bot.sendMessage(chatId, JSON.stringify(data))
 			
 			const user = await User.findOne({ userId: chatId })
 			if (!user) {
@@ -189,73 +239,3 @@ bot.on('message', async (msg) => {
 		}
 	}
 })
-
-
-// Кнопка Аренды
-// bot.onText(/\Аренда/, async (msg) => {
-// 	const chatId = msg.chat.id
-// 	await bot.sendMessage(chatId, 'По данной услуге аналитика невозможно, обратитесь в службу поддержки')
-// })
-
-// bot.onText(/\В розницу/, async (msg) => {
-// 	const chatId = msg.chat.id
-// 	const user = await User.findOne({ userId: chatId })
-// 	if (!user) {
-// 		const newUser = User({
-// 			userId: chatId,
-// 			businessType: 'product_retail'
-// 		})
-// 		newUser.save().then(async (res) => {
-// 			await getWebAppButton(chatId)
-// 		})
-// 	} else {
-// 		await User.findOneAndUpdate({ userId: chatId }, {
-// 			businessType: 'product_retail'
-// 		}).then(async (res) => {
-// 			await getWebAppButton(chatId)
-// 		})
-// 	}
-// })
-
-//
-// bot.onText(/\Оптом/, async (msg) => {
-// 	const chatId = msg.chat.id
-// 	const user = await User.findOne({ userId: chatId })
-// 	if (!user) {
-// 		const newUser = User({
-// 			userId: chatId,
-// 			businessType: 'product_wholesale'
-// 		})
-// 		newUser.save().then(async (res) => {
-// 			await getWebAppButton(chatId)
-// 		})
-// 	} else {
-// 		await User.findOneAndUpdate({ userId: chatId }, {
-// 			businessType: 'product_wholesale'
-// 		}).then(async (res) => {
-// 			await getWebAppButton(chatId)
-// 		})
-// 	}
-// })
-
-
-// bot.onText(/\Услуга/, async (msg) => {
-// 	const chatId = msg.chat.id
-// 	const user = await User.findOne({ userId: chatId })
-//
-// 	if (!user) {
-// 		const newUser = User({
-// 			userId: chatId,
-// 			businessType: 'service'
-// 		})
-// 		newUser.save().then(async (res) => {
-// 			await getWebAppButton(chatId, 'service')
-// 		})
-// 	} else {
-// 		await User.findOneAndUpdate({ userId: chatId }, {
-// 			businessType: 'service'
-// 		}).then(async (res) => {
-// 			await getWebAppButton(chatId, 'service')
-// 		})
-// 	}
-// })
